0c0bd97e02f8092e1fa49c0daf80282e93f06b4a
Zehra <50207648+Zerzavot@users.noreply.github.com>
2022-10-11 08:58:19 +0300

    Add files via upload

---
+++ Week3/oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
@@ -0,0 +1,9 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class JdbcProductDao implements ProductDao{
+
+    @Override
+    public void add(Product product){System.out.println(" Product added with JDBC to db");}
+}
